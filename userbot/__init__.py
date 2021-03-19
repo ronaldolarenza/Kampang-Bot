9e802578b16a9438f86c0a9b8cbfa53b791f8065
Koala <72527434+ManusiaRakitan@users.noreply.github.com>
2021-03-19 10:10:28 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -171,7 +171,7 @@ BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "KAMPANG-BOT")
 
 # Bot version
-BOT_VER = os.environ.get("BOT_VER", "4.0")
+BOT_VER = os.environ.get("BOT_VER", "Kampang 4.6")
 
 # Default .alive username
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
